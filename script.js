05c2aba7bd2fbd57608eb5e14893a3c4ddad1510
gtkrprivateserver <gtkrprivateserver@gmail.com>
2025-11-10 01:54:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const menuBtn = document.querySelector(".menu-btn");
-const sidebar = document.querySelector(".sidebar");
+const menuBtn = document.querySelector('.menu-btn');
+const sidebar = document.querySelector('.sidebar');
 
-menuBtn.addEventListener("click", () => {
-  sidebar.classList.toggle("active");
-});
+if (menuBtn && sidebar) {
+  menuBtn.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+  });
+}
