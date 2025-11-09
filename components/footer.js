edee54f2746663c0ac7c88ed503eb362ca7f6694
gtkrprivateserver <gtkrprivateserver@gmail.com>
2025-11-10 00:39:21 +0700

    Create footer.js

---
+++ components/footer.js
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-900 text-white py-8 text-center space-y-4">
+      <div className="flex justify-center space-x-6">
+        <a href="#" className="hover:text-blue-600 transition">Facebook</a>
+        <a href="#" className="hover:text-blue-600 transition">Instagram</a>
+        <a href="#" className="hover:text-blue-600 transition">LinkedIn</a>
+        <a href="#" className="hover:text-blue-600 transition">Twitter</a>
+      </div>
+      <p>&copy; 2025 KNET Entertainment. All rights reserved.</p>
+    </footer>
+  );
+}
